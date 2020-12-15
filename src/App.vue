de6740898a7ace1d145fa2cb5f6f2853c0ad4e8b
wdu01 <wdu01@etud.u-pem.fr>
2020-12-15 16:13:28 +0100

    定义Train组件

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Train begin="18:03" dest="Kiruna" end="09:11" origin="Sthlm" />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Train from "@/components/Train";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Train,
+    HelloWorld,
   }
 }
 </script>
---
+++ src/components/Train.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="train">
+        <p>{{origin}} -- {{dest}}</p>
+        <p>{{begin}} <b>{{origin}}</b></p>
+        <p>{{end}} <b>{{dest}}</b></p>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "Train",
+        props: {
+            origin: String,
+            dest: String,
+            begin: String,
+            end: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
