1040da09f3bf56633df21c0c88f8b6f827ae8715
wdu01 <wdu01@etud.u-pem.fr>
2020-12-15 16:55:01 +0100

    定义Plane组件

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <Train begin="18:03" dest="Kiruna" end="09:11" origin="Sthlm" />
-  </div>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+        <Train begin="18:03" dest="Kiruna" end="09:11" origin="Sthlm"/>
+        <Plane dest="Amsterdam, Schiphol (AMS)" v-bind:dest-time="new Date()" flight-num="AF1640" company="Air France"
+               depart="Paris, Paris Charles De Gaulle Airport " v-bind:depart-time="new Date()"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import Train from "@/components/Train";
+    import HelloWorld from './components/HelloWorld.vue'
+    import Train from "@/components/Train";
+    import Plane from "@/components/Plane";
 
-export default {
-  name: 'App',
-  components: {
-    Train,
-    HelloWorld,
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            Plane,
+            Train,
+            HelloWorld,
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/Plane.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="plane">
+        <p>{{flightNum}} {{company}}</p>
+        <p>{{departTime}} {{depart}}</p>
+        <p>{{destTime}} {{dest}}</p>
+    </div>
+    
+</template>
+
+<script>
+    export default {
+        name: "Plane",
+        props:{
+            depart:String,
+            departTime:String,
+            dest:String,
+            destTime:String,
+            company:String,
+            flightNum:String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
