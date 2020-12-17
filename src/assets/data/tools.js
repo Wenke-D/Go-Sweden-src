f409a3ffa44262c0bf5f853a42c61232e442b4f1
Wenke-D <matthieu.du@outlook.com>
2020-12-17 20:09:11 +0100

    更新day0的内容

--- src/assets/data/tools.js
+++ src/assets/data/tools.js
@@ -1,3 +1,6 @@
 export default [
-    "毛巾", "床单", "法国电源转换头", "拖鞋", "耳机"
+    {content: "Abisko住宿不提供毛巾及床上用品(bed linen and towels)，租借费用SEK 225 per person"},
+    {content: "瑞典和法国的电源接口相同"},
+    {content: "瑞典进一步加强了防疫政策，但没有值得担心的影响"},
+    {content: "戴高乐机场免税店没有关闭", url:"https://www.parisaeroport.fr/passagers/shopping/boutiques-et-restaurants"},
 ]
--- src/components/Day0.vue
+++ src/components/Day0.vue
@@ -5,8 +5,10 @@
 
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <span class="title">文件</span>
-                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                <span class="title">Docs à préparer</span>
+                <el-button style="float: right; padding: 3px 0" type="text">
+                    <el-link type="warning" href="tel:+33766684919">需要打印？</el-link>
+                </el-button>
             </div>
             <ul>
                 <li v-for="(value, index) in docs" :key="index" class="text item">
@@ -16,12 +18,23 @@
         </el-card>
         <el-card class="box-card">
             <div slot="header" class="clearfix">
-                <span class="title">日用品</span>
-                <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+                <span class="title">Notice</span>
+                <el-button style="float: right; padding: 3px 0" type="text">
+                    <el-link type="warning"
+                             href="https://www.krisinformation.se/en/hazards-and-risks/disasters-and-incidents/2020/official-information-on-the-new-coronavirus/visiting-sweden-during-the-covid-19-pandemic">
+                        More info about Covid-19 in Sweden
+                    </el-link>
+                </el-button>
             </div>
-            <div v-for="(value, index) in tools" :key="index" class="text item">
-                {{value}}
-            </div>
+            <ul>
+                <li v-for="(value, index) in tools" :key="index" class="text item">
+                    {{value.content}}
+                    <el-link type="info"
+                             :href="value.url" v-if="value.url!==undefined">
+                        plus de détail
+                    </el-link>
+                </li>
+            </ul>
         </el-card>
     </div>
 </template>
